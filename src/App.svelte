7e335483f8a2a86373aa65e322971f91fe453c5f
NamYoungGon <42319010+ggons@users.noreply.github.com>
2023-07-25 20:16:33 +0900

    .

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,19 @@
 <script>
+  import { onMount } from 'svelte';
   import Router from './router.svelte';
-  // import { auth } from './stores';
+  import { auth, isRefresh } from './stores';
+
+  const refresh_time = 1000 * 60 * 14;
+
+  onMount(() => {
+    const onRefresh = setInterval(() => {
+      if ($isRefresh) {
+        auth.refresh();
+      } else {
+        clearInterval(onRefresh)
+      }
+    }, refresh_time)
+  })
 </script>
 
 <div class="main-container">
